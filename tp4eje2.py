6a788fc7f92d3ed730b735353663a63bbf92a156
TomasSautu <tomassautu@gmail.com>
2021-05-21 19:19:36 -0300

    Ejercicio 2 terminado! (suma lenta)
    termine mi codigo del ejercicio dos en el que tenia que sumar de forma lenta (1+1+1+1... ; 1-1-1-1...)

--- tp4eje2.py
+++ tp4eje2.py
@@ -4,13 +4,27 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-from tp4eje1 import ingreso_entero
+import tp4eje1 as suma
 
-
-
+def suma_lenta(numeroUno, numeroDos):
+    
+    resultadoSuma = numeroUno + numeroDos
+    
+    if numeroUno < resultadoSuma:
+        while numeroUno < resultadoSuma:
+            numeroUno = numeroUno+1
+    else:
+        while numeroUno > resultadoSuma:
+            numeroUno = numeroUno-1
+    return numeroUno
+        
 
 def prueba():
-    pass
+    print("Inserta dos número para ser sumados de forma lenta")
+    numero_1 = suma.ingreso_entero("Inserta el primer número: ")
+    numero_2 = suma.ingreso_entero("Inserta el segundo número: ")
+    resultado = suma_lenta(numero_1, numero_2)
+    print(resultado)
 
 if __name__ == "__main__":
     prueba()

d1c132cf92182f70c10276cb54253d548d8f8a68
TomasSautu <tomassautu@gmail.com>
2021-05-21 17:52:26 -0300

    Nuevo archivo del ejercicio 2
    Borré el anterior e hice este por un error con el anterior

---
+++ tp4eje2.py
@@ -0,0 +1,16 @@
+################
+# Tomás Sautú - @TomasSautu
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from tp4eje1 import ingreso_entero
+
+
+
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()

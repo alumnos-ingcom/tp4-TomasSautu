f377103c0171431816d8d237a26221147f0bacf3
TomasSautu <tomassautu@gmail.com>
2021-05-22 18:06:52 -0300

    Comenzando el ejercicio 5
    comienzo del ejercicio 5

---
+++ tp4eje5.py
@@ -0,0 +1,12 @@
+################
+# Tomás Sautú- @TomasSautu
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()

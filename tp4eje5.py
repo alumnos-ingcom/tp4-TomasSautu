3849c30d6806894f426cc8659053940b076e9df8
TomasSautu <tomassautu@gmail.com>
2021-05-22 18:16:56 -0300

    Ejercicio 5 terminado! (signo positivo o negativo)
    código que pide el ingreso de un número para comprobar si es un numero positivo o negativo y devolverlo como respuesta, en caso de ser cero devuelve cero como respuesta.

--- tp4eje5.py
+++ tp4eje5.py
@@ -4,9 +4,22 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+from tp4eje1 import ingreso_entero
+
+def signo(numero):
+    
+    if (numero == 0):
+        return "El número es cero"
+    if (numero > 0):
+        return "El número es positivo (+)"
+    else:
+        return "El número es negativo (-)"
 
 def prueba():
-    pass
+    numero = ingreso_entero("Ingrese un número para decirle si es positivo o negativo: ")
+    valor = signo(numero)
+    print(valor)
+    
 
 if __name__ == "__main__":
     prueba()

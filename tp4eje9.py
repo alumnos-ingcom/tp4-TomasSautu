49ec6f3f6c0db494e8155d823e5aa084981cecf5
TomasSautu <tomassautu@gmail.com>
2021-05-23 15:40:23 -0300

    Ejercicio 9 terminado! (Número primos)
    Programa que pide el ingreso de un número entero y que devuelve un mensaje si es primo y si no es primo  devuelve un mensaje diciéndolo.

---
+++ tp4eje9.py
@@ -0,0 +1,26 @@
+################
+# Tomás Sautú- @TomasSautu
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from tp4eje1 import ingreso_entero
+
+def es_primo(numero):
+    for i in range(2,numero):
+        if numero % i == 0 :
+            return False
+        else:
+            return True
+
+
+def prueba():
+    numero = ingreso_entero("Ingrese un número para saber si es primo: ")
+    primo = es_primo(numero)
+    if primo == True:
+        print(f"El número {numero} es primo")
+    else:
+        print(f"El numero {numero} no es primo")
+
+if __name__ == "__main__":
+    prueba()

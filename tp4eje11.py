977984f354aef9362828d862945c25d4c538e34c
TomasSautu <tomassautu@gmail.com>
2021-05-23 17:01:46 -0300

    Empezando ejercicio 11
    comenzando a hacer el ejercicio 11 sobre palindromos

---
+++ tp4eje11.py
@@ -0,0 +1,14 @@
+################
+# Tomás Sautú- @TomasSautu
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def palindromo(texto):
+    
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()

173fc1f3fb6668e8e4baadb2a4fdd73aa1354b47
TomasSautu <tomassautu@gmail.com>
2021-06-03 15:52:36 -0300

    Ejercicio 11 terminado! (Texto Palíndromo)
    Este programa pide el ingreso de un texto, luego ese texto es enviado a una función que se encargará de analizar el texto, convirtiendo todo a minúscula y sin espacios, luego comparando primera la mitad con la segunda mitad invertida para ver si son iguales, si lo son retorna True, si no lo es retorna False.
    En la función prueba() se ejecuta la respuesta según lo que retornó la función.

--- tp4eje11.py
+++ tp4eje11.py
@@ -4,11 +4,31 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+from tp4eje1 import ingreso_entero
+
 def palindromo(texto):
+    texto = texto.replace(" ","")
+    texto = texto.lower
+    largo = len(texto)
     
+    if largo % 2 == 0:
+        izquierda = texto[:largo // 2]
+        derecha = texto[largo // 2:]
+    else:
+        izquierda = texto[:largo// 2]
+        derecha = texto[largo // 2 + 1:]
+        
+    return izquierda == derecha[::-1]
+
 
 def prueba():
-    pass
+    texto = input("Ingrese una palabra o texto para saber si es palindromo o no: ")
+    resultado = palindromo(texto)
+    if resultado == True:
+        print(f'"{texto}" es un palindromo')
+    else:
+        print(f'"{texto}" no es palindromo')
+
 
 if __name__ == "__main__":
     prueba()
